1d4012f4dde467b1391c6c8ef795137abbfde18d
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-07-19 15:32:33 +0700

    เรียนประกาศตัวแปร

---
+++ ep1/index.ts
@@ -0,0 +1,10 @@
+const firstName: string = 'ภูวนาถ ไชยวิเศษ'
+
+var y: number = 10
+y = 20
+
+var ต้องการกลับบ้าน: boolean = true
+
+console.log(firstName)
+console.log(y)
+console.log(ต้องการกลับบ้าน)
