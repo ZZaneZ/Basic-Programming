814d2e8d9a018b506aabb27d0a4d0a3526872178
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-07-19 16:37:19 +0700

    Comment

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,21 +1,21 @@
-const firstName: string = 'ภูวนาถ ไชยวิเศษ'
-var y: number = 10
+const firstName: string = 'ภูวนาถ ไชยวิเศษ' // กำหนดค่าคงตัวให้ตัวแปรแบบ string ให้ firstName
+var y: number = 10 // กำหนดค่าให้ตัวแปรให้ y
     y = 20
-var iWantToGoHome: boolean = true
+var iWantToGoHome: boolean = true // กำหนดตัวแปรค่าความจริงให้ iWantToGoHome
 
-console.log(firstName)
+console.log(firstName) // แสดงผล firstName
 
-console.log(y)
+console.log(y) // แสดงผล y
 
-console.log(iWantToGoHome)
+console.log(iWantToGoHome) //แสดงผล iWantToGoHome
 
 
 
-const a: number = 5
-const b: number = 10
+const a: number = 5 // กำหนดค่าคงตัวให้ตัวแปรแบบ number ให้ a
+const b: number = 10 // กำหนดค่าคงตัวให้ตัวแปรแบบ number ให้ b
 
-console.log(a === b)
+console.log(a === b) // แสดงผล a เท่ากับ b?
 
-console.log(a > b)
+console.log(a > b) // แสดงผล a > b?
 
-console.log(a <= b)
+console.log(a <= b) // แสดงผล a <= b?
