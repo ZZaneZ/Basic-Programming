b74cd45b8bd0bfb6ff431a83554b77c9f56bc7c1
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-07-19 16:00:14 +0700

    แบบฝึกหัด

--- ep1/index.ts
+++ ep1/index.ts
@@ -8,3 +8,12 @@ var ต้องการกลับบ้าน: boolean = true
 console.log(firstName)
 console.log(y)
 console.log(ต้องการกลับบ้าน)
+
+const a: number = 5
+const b: number = 10
+
+console.log(a === b)
+
+console.log(a>b)
+
+console.log(a<=b)
