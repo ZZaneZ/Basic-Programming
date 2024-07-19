30b996be05bb000eac7089e9fc6c816d2fe97603
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-07-19 16:24:23 +0700

    แบบฝึกหัด

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,13 +1,13 @@
 const firstName: string = 'ภูวนาถ ไชยวิเศษ'
 var y: number = 10
     y = 20
-var ต้องการกลับบ้าน: boolean = true
+var iWantToGoHome: boolean = true
 
 console.log(firstName)
 
 console.log(y)
 
-console.log(ต้องการกลับบ้าน)
+console.log(iWantToGoHome)
 
 
 
