0d2971b3799ad3eeddc82a0a7e7fd63bd3814092
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-08-09 14:46:29 +0700

    Slice

---
+++ ep5/index.ts
@@ -0,0 +1,35 @@
+const fName: string = "นายภูวนาถ ไชยวิเศษ"
+
+console.log(fName.slice(fName.indexOf('ไชยวิเศษ'), fName.indexOf('ไชยวิเศษ') + 'ไชยวิเศษ'.length ))
+
+
+
+const fName: string = "นายภูวนาถ ไชยวิเศษ"
+
+console.log(fName.split(" "))
+
+
+
+const fName: string = "test message test"
+
+console.log(fName.replace("test", "null"))
+
+
+
+const fName: string = "test message test"
+
+console.log(fName.replaceAll("test", "null"))
+
+
+
+function getPrefix(Name: string) {
+    if (Name.includes('นาย')) {
+        return 'นาย'
+    } else if (Name.includes('นางสาว')) {
+        return 'นางสาว'
+    } else {
+        return 'null'
+    }
+}
+
+console.log(getPrefix('นายภูวนาถ ไชยวิเศษ'))
