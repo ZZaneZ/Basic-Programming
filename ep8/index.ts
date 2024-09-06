e8d57757fb031a7504dc8509ad901ca794f9edc3
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-09-06 13:41:27 +0700

    Array

---
+++ ep8/index.ts
@@ -0,0 +1,17 @@
+const aboutMe = {
+    firstName : 'Test',
+    lastName : 'Test',
+    nickName : 'Test',
+    age : 20,
+    phoneNumber : 191,
+    address : 'Mars',
+    postalAddress : 99999,
+    email : 'DoNotHackMe@gmail.com',
+    bestFriend : {
+        bFirstName : 'Robert',
+        bLastName : 'Downney Jr',
+        bTitle : 'Victor Von Doom',
+    }
+}
+
+console.log(aboutMe.bestFriend.bTitle)
