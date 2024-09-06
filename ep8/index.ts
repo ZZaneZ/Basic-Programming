3c21b8313c3f5f819f7924412b36f9a899f5b3d8
ZZaneZ <71954443+ZZaneZ@users.noreply.github.com>
2024-09-06 15:49:59 +0700

    Hw

--- ep8/index.ts
+++ ep8/index.ts
@@ -15,3 +15,40 @@ const aboutMe = {
 }
 
 console.log(aboutMe.bestFriend.bTitle)
+
+
+
+const myFriends = [{
+    firstName : 'Lisa',
+    lastName : 'Bp',
+    AKA : 'LookChin',
+    age : 16,
+}, { 
+    firstName : 'Rose',
+    lastName : 'Bp',
+    AKA : 'Chipmunk',
+    age : 18,
+}, { 
+    firstName : 'Jisoo',
+    lastName : 'Bp',
+    AKA : 'Angel',
+    age : 20,
+}, {  
+    firstName : 'Jennie',
+    lastName : 'Bp',
+    AKA : 'Solo',
+    age : 22,
+}]
+
+const myFriendsAKA = myFriends.map(function(element, index) {
+    return ({
+        name: element.firstName,
+        title: element.AKA,
+    })
+})
+
+const myFriendsMoreThan = myFriends.filter(function(element, index) {
+    return element.age > 19 && element.lastName == 'Bp'
+})
+
+console.log(myFriendsMoreThan)
